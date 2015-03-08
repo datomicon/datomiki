29108a39ce9d185bb78aff977127c543491b2f4a
Orlin M Bozhinov <om@soundsapiens.com>
2015-03-08 20:25:25 +0200

    get the npm script names from package.json, no need to list them all explicitly

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp')
 var shell = require('gulp-shell')
+var keys = require('lodash').keys
 
-var scripts = ['build', 'build:min', 'start', 'test']
+// counting on the presence of 'build' and 'start'
+var scripts = keys(require('./package.json').scripts)
 
 scripts.forEach(function(script) {
   gulp.task(script, shell.task('npm run ' + script))

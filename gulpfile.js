b4e8c61119c3938c099c93e5b39c7050fd3fd410
Orlin M Bozhinov <om@soundsapiens.com>
2015-03-10 23:30:01 +0200

    using childish-process.run and node-notifier to get notified about npm test status

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp')
 var shell = require('gulp-shell')
 var keys = require('lodash').keys
+var run = require('childish-process').run
+var notifier = require('node-notifier')
 
 // counting on the presence of 'build' and 'start'
 var scripts = keys(require('./package.json').scripts)
@@ -9,7 +11,19 @@ scripts.forEach(function(script) {
   gulp.task(script, shell.task('npm run ' + script))
 })
 
-gulp.task('test:watch', function(){
+// override the test script task
+gulp.task('test', function() {
+  run("npm test", {eventHandlers: {close: function(code) {
+      if (code === 0) {
+        notifier.notify({message: 'The tests have passed.'})
+      }
+      else {
+        notifier.notify({message: 'Tests fail!'})
+      }
+  }}})
+})
+
+gulp.task('test:watch', function() {
   gulp.watch('./datomiki.ki.js', ['test'])
 })
 

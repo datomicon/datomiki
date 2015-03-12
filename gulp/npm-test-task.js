be50190d754ac83d20f742fd11aecef83c9cb29a
Orlin M Bozhinov <om@soundsapiens.com>
2015-03-12 22:41:37 +0200

    gulp/npm-test-task should be suitable for extraction / reuse

--- gulp/npm-test-task.js
+++ gulp/npm-test-task.js
@@ -1,8 +1,7 @@
 var run = require('childish-process').run
 var notifier = require('node-notifier')
 var args = require('yargs')
-  .string("t").alias("t", "--test").describe("n", "tell gulp what to test")
-  .boolean("n").alias("n", "--test-new").describe("n", "only test/new.spec.coffee")
+  .string("t").alias("t", "--test").describe("t", "tell gulp what to test")
   .argv
 
 module.exports = function (event) {
@@ -12,10 +11,8 @@ module.exports = function (event) {
       if (/.spec.coffee$/.test(event.path))
         test += ' ' + event.path
   }
-  else {
-    if (args.t) test += ' ' + args.t
-    if (args.n) test += ' test/new.spec.coffee'
-  }
+  else if (args.t)
+    test += ' ' + args.t
   run(test, {eventHandlers: {close: function(code) {
       if (code === 0) {
         notifier.notify({message: 'The tests have passed.'})

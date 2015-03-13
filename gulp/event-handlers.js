91c51b4d2dee3ecbc3e20b1a2478d7d4ed62d63f
Orlin M Bozhinov <om@soundsapiens.com>
2015-03-13 15:35:52 +0200

    the build:watch handlers becomes generic build handlers, moved to gulp/event-handlers.js

--- gulp/event-handlers.js
+++ gulp/event-handlers.js
@@ -1,22 +1,26 @@
 var notifier = require('node-notifier')
 
 module.exports = {
+  build:
+    {stderr: function(data) {
+      notifier.notify({message: 'The build has failed!'})}
+    },
   test:
     {close: function(code) {
-        if (code === 0) {
-          notifier.notify({message: 'The tests have passed.'})
-        }
-        else {
-          notifier.notify({message: 'Tests fail!'})
-        }
+      if (code === 0) {
+        notifier.notify({message: 'The tests have passed.'})
+      }
+      else {
+        notifier.notify({message: 'Tests fail!'})
+      }
     }},
   "test-part":
     {close: function(code) {
-        if (code === 0) {
-          notifier.notify({message: 'The requested tests have passed.'})
-        }
-        else {
-          notifier.notify({message: 'Some tests fail!'})
-        }
+      if (code === 0) {
+        notifier.notify({message: 'The requested tests have passed.'})
+      }
+      else {
+        notifier.notify({message: 'Some tests fail!'})
+      }
     }}
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,19 @@
 var gulp = require('gulp')
 var run = require('childish-process').run
+var eho = require('./gulp/event-handlers')
 var notifier = require('node-notifier')
 
 // counting on the presence of 'build' and 'start'
 require('./gulp/npm-scripts')(gulp, {exclude: ['test', 'build:watch']})
 
-// extend 'test'; reuse test fn for gulp test:watch
+// modify 'test'; reuse test fn for gulp test:watch
 var test = require('./gulp/test-task')({testsRe: /\.spec\.coffee$/,
                                         testCmd: './node_modules/.bin/mocha'})
 gulp.task('test', test)
 
 // extend 'build:watch'
 gulp.task('build:watch', function() {
-  run('npm run build:watch', {eventHandlers: {
-    stderr: function(data) {
-      notifier.notify({message: 'The build has failed!'})}
-  }})
+  run('npm run build:watch', {eventHandlers: eho.build})
 })
 
 gulp.task('wait-up', function () {

8c1364aefc5291eb7193429d85223754b2e6a657
Orlin M Bozhinov <om@soundsapiens.com>
2015-03-25 15:54:44 +0200

    gulp dev #default task gulp-helps instead; not sure what to do about help for the test tasks yet

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp-npm-run')(require('gulp'), {
+  include: {'start': 'Run the datomic servers. This task doesn\'t exit.'},
   exclude: ['test'],
   require: ['build', 'start'],
   requireStrict: true,
@@ -16,4 +17,6 @@ gulp.task('test:watch', ['wait-up'], function() {
   require('gulp-watch')(['./datomiki.js', 'test/*.spec.coffee'], test)
 })
 
-gulp.task('default', ['start', 'build:watch', 'test:watch'])
+gulp.task('dev', 'Develop datomiki...', ['start', 'build:watch', 'test:watch'])
+
+gulp.task('default', 'Delegates to `gulp help`.', ['help'])
